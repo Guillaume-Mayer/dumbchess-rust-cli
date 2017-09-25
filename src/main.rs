5efb20edf8019ce983ddeb11c61ba549861055b4
Guillaume Mayer <gui77aume@gmail.com>
2017-09-25 04:04:37 -0300

    Better

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate chess;
 
+use chess::Board;
+
 fn main() {
-    let b = chess::chess::Board::new();
-    println!("Hello Chess\n{}", b.to_str());
+    let b = Board::new();
+    println!("Hello Chess\n\n{}", b);
 }
