34ea5ef9a92327cf95e43019fe9d82a30a00cdf1
Guillaume Mayer <gmayer@arkho.tech>
2017-10-04 12:30:49 -0300

    prompt

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ extern crate chess;
 
 use chess::game::Game;
 use std::io;
+use std::io::Write;
 
 fn main() {
     let mut g = Game::new();
     println!("Welcome to DumbChess");
     loop {
+        print!("> ");
+        io::stdout().flush().expect("Failed to flush stdout");
         let mut comm = String::new();
         io::stdin().read_line(&mut comm).expect("Failed to read line");
         let comm = comm.trim();
