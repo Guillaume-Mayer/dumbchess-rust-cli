cd767dd6a2992b4ffc67823ebd8f359866e00735
Guillaume Mayer <gui77aume@gmail.com>
2017-10-04 01:13:41 -0300

    interactive

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 extern crate chess;
 
 use chess::game::Game;
+use std::io;
 
 fn main() {
     let mut g = Game::new();
-    println!("{}", g.to_fen());
-    g.play("e4");
-    println!("{}", g.to_fen());
-    g.play("c5");
-    println!("{}", g.to_fen());
-    g.play("Nf3");
-    println!("{}", g.to_fen());
+    println!("Welcome to DumbChess");
+    loop {
+        let mut comm = String::new();
+        io::stdin().read_line(&mut comm).expect("Failed to read line");
+        let comm = comm.trim();
+        match comm {
+            "quit" | "exit" => break,
+            "new" => g = Game::new(),
+            "fen" => println!("{}", g.to_fen()),
+            "pgn" => println!("{}", g.to_pgn()),
+            _ => g.play(comm),
+        }
+    }
 }
