854e01bd818811af22f7332322fbb0df59e4fe37
Guillaume Mayer <gmayer@arkho.tech>
2017-10-05 19:36:34 -0300

    externalize lib

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-extern crate chess;
-
-use chess::game::Game;
-use std::io;
-use std::io::Write;
+extern crate client;
 
 fn main() {
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
     println!("Welcome to DumbChess {}", VERSION);
-    let mut g = Game::new();
-    loop {
-        print!("> ");
-        io::stdout().flush().expect("Failed to flush stdout");
-        let mut comm = String::new();
-        io::stdin().read_line(&mut comm).expect("Failed to read line");
-        let comm = comm.trim();
-        match comm {
-            "quit" | "exit" => break,
-            "new" => g = Game::new(),
-            "fen" => println!("{}", g.to_fen()),
-            "pgn" => println!("{}", g.to_pgn()),
-            "show" => println!("{}", g.to_str()),
-            _ => g.play(comm),
-        }
-    }
+    client::run();
 }
